ff79e0df379ede32a21eab74b4984cabcab03fff
sendminder <sendminder@gmail.com>
2024-04-28 14:57:19 +0900

    bottom sheet 추가

--- lib/screens/check_screen.dart
+++ lib/screens/check_screen.dart
@@ -1,14 +1,17 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
-
+import 'package:toddle_toddle/states/goals_state.dart';
 import 'package:toddle_toddle/widgets/custom_text.dart';
+import 'package:toddle_toddle/widgets/add_goal_bottom_sheet.dart';
 
 class CheckScreen extends ConsumerWidget {
   const CheckScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final goals = ref.watch(goalsStateProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const CustomText(
@@ -19,15 +22,41 @@ class CheckScreen extends ConsumerWidget {
       ),
       body: Material(
         color: Theme.of(context).colorScheme.background,
-        child: ListView(
+        child: ListView.builder(
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          children: const <Widget>[],
+          itemCount: goals.isEmpty ? 1 : goals.length,
+          itemBuilder: (context, index) {
+            if (goals.isEmpty) {
+              return const Center(
+                child: CustomText(
+                  text: 'no_goals',
+                  textSize: 16,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              );
+            }
+            return ListTile(
+              title: Text(goals[index].name),
+              subtitle: Text(
+                goals[index].startTime.toString(),
+                style: const TextStyle(fontSize: 14),
+              ),
+            );
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet<void>(
+            isScrollControlled: true,
+            context: context,
+            builder: (BuildContext context) {
+              return AddGoalBottomSheet();
+            },
+          );
+        },
+        tooltip: '항목 추가',
         child: const Icon(FluentIcons.add_24_regular),
-        tooltip: 'Add Item',
       ),
     );
   }
---
+++ lib/widgets/add_goal_bottom_sheet.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:toddle_toddle/data/models/schedule.dart';
+import 'package:toddle_toddle/states/goals_state.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:toddle_toddle/data/models/goal.dart';
+import 'package:toddle_toddle/utils/id_generator.dart';
+
+class AddGoalBottomSheet extends ConsumerWidget {
+  String? goalName;
+  DateTime? startDate;
+  String frequency = 'Daily'; // 'Daily', 'Weekly', 'Custom'
+  bool isDaily = true;
+  List<int> selectedDays = [];
+  String notificationTime = '';
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final goals = ref.watch(goalsStateProvider);
+
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      height: 800,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+              labelText: '목표 이름',
+            ),
+            onChanged: (value) {
+              goalName = value;
+            },
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              final DateTime? picked = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(2000),
+                lastDate: DateTime(2025),
+              );
+              if (picked != null && picked != startDate) {
+                startDate = picked;
+              }
+            },
+            child: Text(startDate == null ? '시작 날짜 설정' : startDate.toString()),
+          ),
+          DropdownButton<String>(
+            value: frequency,
+            onChanged: (String? newValue) {
+              frequency = newValue!;
+            },
+            items: <String>['Daily', 'Weekly', 'Custom']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+          // 시간 설정 버튼 추가
+          ElevatedButton(
+            onPressed: () async {
+              final TimeOfDay? pickedTime = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+              );
+              if (pickedTime != null) {
+                final String formattedTime = pickedTime.format(context);
+                notificationTime = formattedTime; // 시간 업데이트
+              }
+            },
+            child: Text('알림 시간 설정: $notificationTime'),
+          ),
+          if (frequency == 'Weekly')
+            Wrap(
+              children: List<Widget>.generate(7, (int index) {
+                String day = '월화수목금토일'[index];
+                return ChoiceChip(
+                    label: Text(day),
+                    selected: selectedDays.contains(day),
+                    onSelected: (bool selected) {
+                      if (selected) {
+                        selectedDays.add(index);
+                      } else {
+                        selectedDays.remove(index);
+                      }
+                    });
+              }),
+            ),
+          ElevatedButton(
+            onPressed: () {
+              if (frequency == 'Weekly') {
+                isDaily = false;
+              }
+              String id = generateUniqueId();
+              Schedule schedule = Schedule(
+                daysOfWeek: selectedDays,
+                notificationTime: notificationTime,
+                startDate: startDate!,
+                isDaily: isDaily,
+              );
+              Goal newGoal = Goal(
+                id: id,
+                name: goalName!,
+                startTime: startDate!,
+                schedule: schedule,
+              );
+              ref.read(goalsStateProvider.notifier).addOrUpdateGoal(newGoal);
+              Navigator.pop(context);
+            },
+            child: Text('추가하기'),
+          ),
+        ],
+      ),
+    );
+  }
+}
